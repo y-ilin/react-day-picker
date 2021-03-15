ff08776d53499089aa8140037ced9657766231fe
gpbl <io@gpbl.dev>
2021-03-15 07:41:30 -0500

    Add IconDropdown test

---
+++ packages/react-day-picker/src/components/IconNext/IconDropdown.test.tsx
@@ -0,0 +1,16 @@
+import { RenderResult } from '@testing-library/react';
+import React from 'react';
+
+import { customRender } from 'test';
+
+import { StyledComponentProps } from 'types';
+
+import { IconNext } from './IconNext';
+
+const setup = (props?: StyledComponentProps): RenderResult => {
+  return customRender(<IconNext {...props} />);
+};
+test('should render correctly', () => {
+  const { container } = setup({ className: 'foo', style: { color: 'red' } });
+  expect(container.firstChild).toMatchSnapshot();
+});
